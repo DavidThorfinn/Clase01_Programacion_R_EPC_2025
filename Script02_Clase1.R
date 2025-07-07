a9839c4d64cc8d3eb92daa907e7aa2df81918c21
DavidThorfinn <alvarodavid9713@gmail.com>
2025-07-07 16:28:03 +0000

    Agregamos ciertos servicios en linea que ofrecen la capacidad de interpretar codigo del lenguaje de programacion R

--- Script02_Clase1.R
+++ Script02_Clase1.R
@@ -2,3 +2,14 @@
 #### Herramientas en linea para programar con R ####
 # Posit Cloud
 # Notebook de Google Colab
+# Notebook de kaggle
+# MyBinder
+# Deepnote
+# PapaerSpace
+# SageMaker (AWS)
+# Watson Studio (IBM)
+# CoCalc
+# SageMath
+# mljar
+# LabEx
+# Fin
