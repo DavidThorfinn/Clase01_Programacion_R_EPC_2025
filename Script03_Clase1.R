44577835eb5fd6c27f266d49bce6e6b5a7464ee9
DavidThorfinn <alvarodavid9713@gmail.com>
2025-07-07 17:01:02 +0000

    Algunos paquetes base y populares a tener en cuenta

---
+++ Script03_Clase1.R
@@ -0,0 +1,11 @@
+# Paquetes base:
+  # stats: Funciones estadisticas basicas
+  # graphics: Herramientas para graficos base (estandar)
+  # utils: Utilidades para manipulacion de datos e instalacion de paquetes
+
+# Paquetes populares
+  # ggplot2
+  # dplyr
+  # caret
+  # shiny
+  # forecast
