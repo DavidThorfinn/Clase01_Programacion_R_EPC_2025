8f576398d2efe682f2a7dc4ac10c8bc6ebf9aa16
Alvaro David Mendoza Huaylinos <162851394+DavidThorfinn@users.noreply.github.com>
2025-07-06 21:55:46 -0500

    Archivos creados en la infraestructura de posit

---
+++ Script01_Clase1.R
@@ -0,0 +1,15 @@
+# Primer comentario
+# En esta sesion mostraremos algunas e las cpacidades del entorno
+# de desarrollo intregaro(IDE) posit (Ex-Rstudio)
+
+#### Introduccion al Lenguaje de Programacion R ####
+# R es un lenguaje de programacion y entornos de software libre diseñado especificamente
+# para analisis estadistico, visualizacion de datos y modelado computacional.
+
+#### Origen ####
+# Origen es de los 90's
+# Objetivo inicial: Proporcionar una alternativa flexible y accesible al entorno S
+
+#### Reconocimiento actual ####
+# Es uno de lso lenguajes mas utilizados en el analisis de datos junto con Python
+# Tiene miles de paquetes disponibles a traves de CRAM
